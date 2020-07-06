8047ddc97b57f6acdf78bb0ab8e4a9d23f16d66b
Jude Okagu <juud0010@gmail.com>
2020-07-06 21:15:23 +0200

    user-model

--- travel-backend/app.js
+++ travel-backend/app.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send({ msg: 'hello from react' });
+});
+
+//  connection to db
+
+mongoose
+  .connect(process.env.DATABASE, { useNewUrlParser: true })
+
+  .then(() => console.log('DB Connected'));
+
+mongoose.connection.on('error', (err) => {
+  console.log(`DB connection error: ${err.message}`);
 });
 
 const port = process.env.PORT || 8000;
