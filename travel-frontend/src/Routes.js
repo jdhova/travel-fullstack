49774b32be59de298a86d8b4e5e7656fbb580450
Jude Okagu <juud0010@gmail.com>
2020-07-13 04:08:08 +0200

    working on front

---
+++ travel-frontend/src/Routes.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { BrowserRouter, Switch, Link } from 'react-router-dom';
+
+const Routes = () => {
+  return <div>here we move</div>;
+};
+
+export default Routes;
