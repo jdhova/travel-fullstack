2b7d37d516e1a7a327ade215a3853ac49593011b
Jude Okagu <juud0010@gmail.com>
2020-07-16 15:20:58 +0200

    workingh on signin

--- travel-frontend/src/auth/index.js
+++ travel-frontend/src/auth/index.js
@@ -16,3 +16,38 @@ export const signup = (user) => {
       console.log(err);
     });
 };
+
+export const signin = (user) => {
+  return fetch(`${API}/sigin`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export const authenticate = (data, next) => {
+  if (typeof window !== 'undefined') {
+    localStorage.setItem('jwt', JSON.stringify(data));
+    next();
+  }
+};
+
+export const isAuthenticated = () => {
+  if (typeof window == 'undefined') {
+    return false;
+  }
+  if (localStorage.getItem('jwt')) {
+    return JSON.parse(localStorage.getItem('jwt'));
+  } else {
+    return false;
+  }
+};
--- travel-frontend/src/user/Signin.js
+++ travel-frontend/src/user/Signin.js
@@ -1,7 +1,202 @@
-import React from 'react';
+// import React, { useState } from 'react';
+// import { signin, authenticate, isAuthenticated } from '../auth';
+// import { Redirect } from 'react-router-dom';
+// import '../App.css';
 
-const Signin = () => {
-  return <div>here we signin</div>;
+// const Signin = () => {
+//   const [values, setValues] = useState({
+//     email: '',
+//     password: '',
+//     error: '',
+//     loading: false,
+//   });
+
+//   const { email, password, loading, error, redirectToReferrer } = values;
+//   const { user } = isAuthenticated();
+
+//   const onChange = (name) => (e) => {
+//     setValues({
+//       ...values,
+//       [name]: e.target.value,
+//     });
+//   };
+
+//   const onSubmit = (e) => {
+//     e.preventDefault();
+//     setValues({ ...values, error: false, loading: true });
+//     signin({ email, password }).then((data) => {
+//       if (data.error) {
+//         setValues({ ...values, error: data.error, loading: false });
+//       } else {
+//         authenticate(data, () => {
+//           setValues({
+//             ...values,
+//             redirectToReferrer: true,
+//           });
+//         });
+//       }
+//     });
+//   };
+
+//   const signInForm = () => (
+//     <form>
+//       <div className='form-group'>
+//         <label className='text-muted'>Email</label>
+//         <input
+//           onChange={onChange('email')}
+//           type='email'
+//           className='form-control'
+//           value={email}
+//         />
+//       </div>
+
+//       <div className='form-group'>
+//         <label className='text-muted'>Password</label>
+//         <input
+//           onChange={onChange('password')}
+//           type='password'
+//           className='form-control'
+//           value={password}
+//         />
+//       </div>
+//       <button onClick={onSubmit} className='btn btn-primary'>
+//         Submit
+//       </button>
+//     </form>
+//   );
+
+//   const showError = () => (
+//     <div
+//       className='alert alert-danger'
+//       style={{ display: error ? '' : 'none' }}
+//     >
+//       {error}
+//     </div>
+//   );
+
+//   const showLoading = () =>
+//     loading && (
+//       <div className='alert alert-info'>
+//         <h2>Loading...</h2>
+//       </div>
+//     );
+
+//   const redirectUser = () => {
+//     if (redirectToReferrer) {
+//       if (user && user.role === 1) {
+//         return <Redirect to='/admin/dashboard' />;
+//       } else {
+//         return <Redirect to='/user/dashboard' />;
+//       }
+//     }
+//     if (isAuthenticated()) {
+//       return <Redirect to='/' />;
+//     }
+//   };
+
+//   return (
+//     <div className='signup'>
+//       <div className='signup1'>
+//         <h2>Sigin </h2>
+//       </div>
+//       {showLoading()}
+//       {showError()}
+//       {redirectUser()}
+
+//       <div className='signup2'>{signInForm()}</div>
+//       {JSON.stringify(values)}
+//     </div>
+//   );
+// };
+
+// export default Signin;
+
+import React, { useState } from 'react';
+import { signin } from '../auth';
+import '../App.css';
+import { Link } from 'react-router-dom';
+
+const Signup = () => {
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+    loading: false,
+    error: '',
+  });
+
+  const { email, password, loading, error } = values;
+
+  const onChange = (name) => (e) => {
+    setValues({ ...values, error: false, [name]: e.target.value });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    setValues({ ...values, error: false });
+
+    signin({ email, password }).then((data) => {
+      // console.log('errer', data.error);
+      if (data.error) {
+        setValues({ ...values, error: data.error, success: false });
+      } else {
+        setValues({
+          ...values,
+          redirectToReferrer: false,
+          loading: true,
+        });
+      }
+    });
+  };
+
+  const signInForm = () => (
+    <form>
+      <div className='form-group'>
+        <label className='text-muted'>Email</label>
+        <input
+          onChange={onChange('email')}
+          type='email'
+          className='form-control'
+          value={email}
+        />
+      </div>
+
+      <div className='form-group'>
+        <label className='text-muted'>Password</label>
+        <input
+          onChange={onChange('password')}
+          type='password'
+          className='form-control'
+          value={password}
+        />
+      </div>
+      <button onClick={onSubmit} className='btn btn-primary'>
+        Submit
+      </button>
+    </form>
+  );
+
+  const showError = () => (
+    <div
+      className='alert alert-danger'
+      style={{ display: error ? '' : 'none' }}
+    >
+      {error}
+    </div>
+  );
+
+  return (
+    <div className='signup'>
+      <div className='signup1'>
+        <h2>Signup </h2>
+      </div>
+
+      {showError()}
+
+      <div className='signup2'>{signInForm()}</div>
+      {JSON.stringify(values)}
+    </div>
+  );
 };
 
-export default Signin;
+export default Signup;
