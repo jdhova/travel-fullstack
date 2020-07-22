698fe3be020c0b1ede505d98b06b340c8d25fbb4
Jude Okagu <juud0010@gmail.com>
2020-07-22 19:21:59 +0200

    fixing

--- travel-frontend/src/Routes.js
+++ travel-frontend/src/Routes.js
@@ -8,6 +8,7 @@ import Dashboard from './user/UserDashboard';
 import AdminRoute from './auth/AdminRoute';
 import AdminDashboard from './user/AdminDashboard';
 import AddEvent from './admin/AddEvent';
+import UpdateEvent from './admin/UpdateEvent';
 
 const Routes = () => {
   return (
@@ -18,6 +19,11 @@ const Routes = () => {
         <Route exact path='/Signin' component={Signin} />
         <PrivateRoute path='/user/dashboard' exact component={Dashboard} />
         <AdminRoute path='/admin/dashboard' exact component={AdminDashboard} />
+        <AdminRoute
+          path='/admin/event/update/:eventId'
+          exact
+          component={UpdateEvent}
+        />
 
         <AdminRoute path='/create/event' exact component={AddEvent} />
         {/* <AdminRoute path='/create/trip' exact component={AddTrip} />  */}
--- travel-frontend/src/admin/updateEvent.js
+++ travel-frontend/src/admin/updateEvent.js
@@ -5,12 +5,16 @@
 // import { getCategory, updateCategory } from './apiAdmin';
 // // {category: ["5cd0258f2793ec6e100bc191"], price: []}
 // // http://localhost:3000/admin/category/update/5cd0258f2793ec6e100bc191
-// const UpdateCategory = ({ match }) => {
+
+// const UpdateEvent = ({ match }) => {
 //   const [values, setValues] = useState({
 //     name: '',
 //     error: '',
 //     redirectToProfile: false,
 //     formData: '',
+//     photo: '',
+//     description: '',
+//     loading: false,
 //   });
 
 //   // destructure user and token from localStorage
@@ -18,32 +22,46 @@
 
 //   const { name, error, redirectToProfile } = values;
 
-//   const init = (categoryId) => {
-//     getCategory(categoryId, token).then((data) => {
-//       if (data.error) {
-//         setValues({ ...values, error: data.error });
-//       } else {
-//         // populate the state
-//         setValues({
-//           ...values,
-//           name: data.name,
-//         });
-//       }
-//     });
-//   };
+//   //   const init = (categoryId) => {
+//   //     getCategory(categoryId, token).then((data) => {
+//   //       if (data.error) {
+//   //         setValues({ ...values, error: data.error });
+//   //       } else {
+//   //         // populate the state
+//   //         setValues({
+//   //           ...values,
+//   //           name: data.name,
+//   //         });
+//   //       }
+//   //     });
+//   //   };
+
+//   //   useEffect(() => {
+//   //     init(match.params.categoryId);
+//   //   }, []);
+
+//   //   const handleChange = (name) => (event) => {
+//   //     setValues({ ...values, error: false, [name]: event.target.value });
+//   //   };
+//   // useEffect(() => {
+//   //     init(match.params.productId);
+//   // }, []);
 
 //   useEffect(() => {
-//     init(match.params.categoryId);
+//     setValues({ ...values, formData: new FormData() });
 //   }, []);
 
 //   const handleChange = (name) => (event) => {
-//     setValues({ ...values, error: false, [name]: event.target.value });
+//     const value = name === 'photo' ? event.target.files[0] : event.target.value;
+//     formData.set(name, value);
+//     setValues({ ...values, [name]: value });
 //   };
 
 //   const submitCategoryForm = (e) => {
 //     e.preventDefault();
 //     // update with ? you should send category name otherwise what to update?
 //     const category = {
+//       name: name,
 //       name: name,
 //     };
 //     updateCategory(match.params.categoryId, user._id, token, category).then(
@@ -147,4 +165,199 @@
 //   );
 // };
 
-// export default UpdateCategory;
+// export default UpdateEvent;
+
+import React, { useState, useEffect, Fragment } from 'react';
+
+import { isAuthenticated } from '../auth';
+import { Link, Redirect } from 'react-router-dom';
+import { getProduct, getCategories, updateEvent } from './apiAdmin';
+
+const UpdateEvent = ({ match }) => {
+  const [values, setValues] = useState({
+    name: '',
+    description: '',
+    photo: '',
+    loading: false,
+    error: false,
+    createdEvent: '',
+    redirectToProfile: false,
+    formData: '',
+  });
+  // const [categories, setCategories] = useState([]);
+
+  const { user, token } = isAuthenticated();
+  const {
+    name,
+    description,
+    // price,
+    // // categories,
+    // category,
+    // shipping,
+    // quantity,
+    loading,
+    error,
+    createdEvent,
+    redirectToProfile,
+    formData,
+  } = values;
+
+  //   const init = (productId) => {
+  //     getProduct(productId).then((data) => {
+  //       if (data.error) {
+  //         setValues({ ...values, error: data.error });
+  //       } else {
+  //         // populate the state
+  //         setValues({
+  //           ...values,
+  //           name: data.name,
+  //           description: data.description,
+  //           price: data.price,
+  //           category: data.category._id,
+  //           shipping: data.shipping,
+  //           quantity: data.quantity,
+  //           formData: new FormData(),
+  //         });
+  //         // load categories
+  //         initCategories();
+  //       }
+  //     });
+  //   };
+
+  // load categories and set form data
+
+  //   const initCategories = () => {
+  //     getCategories().then((data) => {
+  //       if (data.error) {
+  //         setValues({ ...values, error: data.error });
+  //       } else {
+  //         setCategories(data);
+  //       }
+  //     });
+  //   };
+
+  //   useEffect(() => {
+  //     init(match.params.productId);
+  //   }, []);
+
+  useEffect(() => {
+    setValues({ ...values, formData: new FormData() });
+  }, []);
+
+  const onChange = (name) => (event) => {
+    const value = name === 'photo' ? event.target.files[0] : event.target.value;
+    formData.set(name, value);
+    setValues({ ...values, [name]: value });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, error: '', loading: true });
+
+    updateEvent(match.params.productId, user._id, token, formData).then(
+      (data) => {
+        if (data.error) {
+          setValues({ ...values, error: data.error });
+        } else {
+          setValues({
+            ...values,
+            name: '',
+            description: '',
+            photo: '',
+            loading: false,
+            error: false,
+            redirectToProfile: true,
+            createdEvent: data.name,
+          });
+        }
+      }
+    );
+  };
+
+  const newPostForm = () => (
+    <form className='mb-3' onSubmit={onSubmit}>
+      <h4>Post Photo</h4>
+      <div className='form-group'>
+        <label className='btn btn-secondary'>
+          <input
+            onChange={onChange('photo')}
+            type='file'
+            name='photo'
+            accept='image/*'
+          />
+        </label>
+      </div>
+
+      <div className='form-group'>
+        <label className='text-muted'>Name</label>
+        <input
+          onChange={onChange('name')}
+          type='text'
+          className='form-control'
+          value={name}
+        />
+      </div>
+
+      <div className='form-group'>
+        <label className='text-muted'>Description</label>
+        <textarea
+          onChange={onChange('description')}
+          className='form-control'
+          value={description}
+        />
+      </div>
+
+      <button className='btn btn-outline-primary'>Update Event</button>
+    </form>
+  );
+
+  const showError = () => (
+    <div
+      className='alert alert-danger'
+      style={{ display: error ? '' : 'none' }}
+    >
+      {error}
+    </div>
+  );
+
+  const showSuccess = () => (
+    <div
+      className='alert alert-info'
+      style={{ display: createdEvent ? '' : 'none' }}
+    >
+      <h2>{`${createdEvent}`} is updated!</h2>
+    </div>
+  );
+
+  const showLoading = () =>
+    loading && (
+      <div className='alert alert-success'>
+        <h2>Loading...</h2>
+      </div>
+    );
+
+  const redirectUser = () => {
+    if (redirectToProfile) {
+      if (!error) {
+        return <Redirect to='/' />;
+      }
+    }
+  };
+
+  return (
+    <Fragment>
+      <h2>{`G'day ${user.name}, ready to add a new product?`}</h2>
+      <div className='row'>
+        <div className='col-md-8 offset-md-2'>
+          {showLoading()}
+          {showSuccess()}
+          {showError()}
+          {newPostForm()}
+          {redirectUser()}
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default UpdateEvent;
