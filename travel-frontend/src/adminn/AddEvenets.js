f8412d85a4093cceaf7d4474c150c234dc903836
Jude Okagu <juud0010@gmail.com>
2020-09-12 20:30:05 +0200

    ff

--- travel-frontend/src/adminn/AddEvenets.js
+++ travel-frontend/src/adminn/AddEvenets.js
@@ -2,14 +2,14 @@ import React, { useState } from 'react';
 import { isAuthenticated } from '../auth';
 import { Link } from 'react-router-dom';
 
-const AddEvenets = () => {
-  const [name, setName] = useState('');
-  const [error, setError] = useState(false);
-  const [success, setSuccess] = useState(false);
+// const AddEvenets = () => {
+//   const [name, setName] = useState('');
+//   const [error, setError] = useState(false);
+//   const [success, setSuccess] = useState(false);
 
-  const { user, token } = isAuthenticated();
+//   const { user, token } = isAuthenticated();
 
-  return <div></div>;
-};
+//   return <div></div>;
+// };
 
 export default AddEvenets;
