a800b0a31d109b8b24ba9d72e27320af672df77b
Jude Okagu <juud0010@gmail.com>
2020-09-27 22:48:08 +0200

    ff

--- travel-frontend/src/core/Events.js
+++ travel-frontend/src/core/Events.js
@@ -34,6 +34,7 @@ const Events = () => {
 
   return (
     <div>
+      {/* <h3 className='mb-5'>Newest Events</h3> */}
       <h3 className='mb-5'>Newest Events</h3>
       <div className='row'>
         {eventsByArrival.map((event, i) => {
