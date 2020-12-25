ab308f7fcffdd6fa89888229cf569e46c6926131
Jude Okagu <juud0010@gmail.com>
2020-12-26 00:43:19 +0100

    fix

--- travel-frontend/src/user/Signin.js
+++ travel-frontend/src/user/Signin.js
@@ -100,9 +100,9 @@ const Signin = () => {
         return <Redirect to='/user/dashboard' />;
       }
     }
-    if (isAuthenticated()) {
-      return <Redirect to='/' />;
-    }
+    // if (isAuthenticated()) {
+    //   return <Redirect to='/' />;
+    // }
   };
 
   return (
